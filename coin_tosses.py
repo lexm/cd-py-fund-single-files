0aee8133bf9af31cd56017bcb3399ca547cfd7ca
Lex Myers <lex.myers@gmail.com>
2017-09-05 15:33:43 -0700

    solution to Coin Tosses

---
+++ coin_tosses.py
@@ -0,0 +1,17 @@
+import random
+def coin_tosses(num):
+    count = {
+        "head": 0,
+        "tail": 0
+        }
+    line = "Attempt #{}: Throwing a coin... It's a {}! ... "
+    line += "Got {} head(s) so far and {} tail(s) so far"
+    for i in range(1, num + 1):
+        toss = random.randint(0,1)
+        if toss == 0:
+            toss_str = "head"
+        else:
+            toss_str = "tail"
+        count[toss_str] += 1
+        print line.format(i, toss_str, count["head"], count["tail"])
+coin_tosses(5000)
